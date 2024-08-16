42a64e319d48f292dad243122fee67a5defdf67b
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-08-16 11:37:42 +0700

    Keyframes

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,14 @@ export default function App() {
         transition={{ duration: 0.1 }}
         className="bg-black size-12"
       />
+      <motion.div
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%', '20%']
+        }}
+        className="size-12 bg-slate-500"
+      />
     </>
   )
 }
